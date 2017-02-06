bb4e9b518d020e28748e8e0427ed7a497916efc6
Alexander Rimasheuski <Squire_86@mail.ru>
2017-02-06 23:26:10 +0300

    Change User control Action

--- MyDiplom/MyDiplom/Form1.Designer.cs
+++ MyDiplom/MyDiplom/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.клиентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.контрактыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.клиентыToolStripMenuItem,
+            this.контрактыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // клиентыToolStripMenuItem
+            // 
+            this.клиентыToolStripMenuItem.Name = "клиентыToolStripMenuItem";
+            this.клиентыToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.клиентыToolStripMenuItem.Text = "клиенты";
+            this.клиентыToolStripMenuItem.Click += new System.EventHandler(this.клиентыToolStripMenuItem_Click);
+            // 
+            // контрактыToolStripMenuItem
+            // 
+            this.контрактыToolStripMenuItem.Name = "контрактыToolStripMenuItem";
+            this.контрактыToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.контрактыToolStripMenuItem.Text = "контракты";
+            this.контрактыToolStripMenuItem.Click += new System.EventHandler(this.контрактыToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 711);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.ToolStripMenuItem клиентыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem контрактыToolStripMenuItem;
+        public System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
--- MyDiplom/MyDiplom/Form1.cs
+++ MyDiplom/MyDiplom/Form1.cs
@@ -8,13 +8,67 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace MyDiplom
 {
+   public  delegate void Change (UserControl userControl1, UserControl userControl2);
+   
     public partial class Form1 : Form
     {
+        public UserControl1 userControl1;
+        public UserControl2 userControl2;
+
+
+        //метод переключающий контроллы, удаляющий текущи и добавляющий следующий
+        public void ChangeUserControl(UserControl userControl1, UserControl userControl2)
+        {
+            if (userControl1 != null)
+            {
+                Controls.Remove(userControl1);
+                userControl1.Dispose();
+            }
+            if (userControl2 != null)
+            {
+                Controls.Remove(userControl2);
+                userControl2.Dispose();
+            }
+
+
+
+
+
+        }
+     
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void клиентыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            //ChangeUserControl(userControl1,userControl2);
+            Change change = ChangeUserControl;
+            change(userControl1,userControl2);
+            userControl1 =new UserControl1();
+            this.Controls.Add(userControl1);
+            userControl1.Top = 25;
+            userControl1.Left = 0;
+
+        }
+
+        private void контрактыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        
+            //ChangeUserControl(userControl1, userControl2);
+            Change change = ChangeUserControl;
+            change(userControl1, userControl2);
+            userControl2 =new UserControl2();
+            this.Controls.Add(userControl2);
+            userControl2.Top = 25;
+            userControl2.Left = 0;
+
         }
     }
 }
